9804422615a8597172216bb0c5dea8efc8ee4d63
Nammuuu <88241109+Nammuuu@users.noreply.github.com>
2023-02-18 10:17:33 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -83,10 +83,10 @@ app.use("/api/v2",cart);
 //     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 // }) 
 
- app.use(express.static(path.join("https://fabulous-hotteok-fed63e.netlify.app/")));
+ app.use(express.static(path.join(__dirname, "https://fabulous-hotteok-fed63e.netlify.app/")));
 
 app.get("*",(req,res) =>{
-    res.sendFile(path.resolve("https://fabulous-hotteok-fed63e.netlify.app/"));
+    res.sendFile(path.resolve(__dirname, "https://fabulous-hotteok-fed63e.netlify.app/"));
 }) 
 
 
